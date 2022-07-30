354d69ff159a90b85e8499a64b51cb39653975e8
Afaaq <afaaqmajeed123@gmail.com>
2022-07-30 15:49:21 +0530

    Updates

---
+++ precache-manifest.969c322d016533c0f7abb1bfe90509e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dfb393484f77962f12b",
+    "url": "/islamic/static/css/main.fdc8d0ec.chunk.css"
+  },
+  {
+    "revision": "1dfb393484f77962f12b",
+    "url": "/islamic/static/js/main.1dfb3934.chunk.js"
+  },
+  {
+    "revision": "05b503beb4d93191790c",
+    "url": "/islamic/static/js/1.05b503be.chunk.js"
+  },
+  {
+    "revision": "30a033b3d87dd5ff2f21",
+    "url": "/islamic/static/js/2.30a033b3.chunk.js"
+  },
+  {
+    "revision": "4acda8c5806fa00a492d",
+    "url": "/islamic/static/js/runtime~main.4acda8c5.js"
+  },
+  {
+    "revision": "88de2bf2aab7eb94f33a9cdc32cf1c91",
+    "url": "/islamic/static/media/youtube.88de2bf2.svg"
+  },
+  {
+    "revision": "60788368e9bab8de200847009fefc4cb",
+    "url": "/islamic/index.html"
+  }
+];
